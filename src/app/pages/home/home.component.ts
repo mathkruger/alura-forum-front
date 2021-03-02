f7bc4ac95a5c6bd6f525ca363b010307a318a5a3
Matheus da Silva Kruger <matheusk@ciandt.com>
2021-03-02 19:11:09 -0300

    adicionando resposta de topico e adicionando scroll infinito nos topicos (paginacao)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { PaginationResult } from './../../models/pagination-result';
 import { TopicoListagem } from './../../models/topico';
 import { BaseService } from './../../services/base/base.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -12,17 +12,46 @@ export class HomeComponent implements OnInit {
 
   constructor(private service: BaseService) { }
 
-  topicos: TopicoListagem[];
+  @ViewChild("novoTopico") botaoNovoTopico: ElementRef<HTMLElement>;
+
+  topicos: TopicoListagem[] = [];
+  page = 0;
+  totalPages = 0;
 
   ngOnInit() {
-    this.service.Get<PaginationResult<TopicoListagem>>('topicos')
-    .subscribe(item => {
-      this.topicos = item.content;
-    })
+    this.obterTopicos();
+  }
+
+  obterTopicos() {
+    this.service.Get<PaginationResult<TopicoListagem>>('topicos?page=' + this.page)
+      .subscribe(item => {
+        this.topicos = this.topicos.concat(item.content);
+        this.page = item.number;
+        this.totalPages = item.totalPages;
+      });
   }
 
   abrir() {
-    
+
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  avancarPagina($event) {
+    if(
+      this.isScrolledIntoView(this.botaoNovoTopico.nativeElement) &&
+      this.page < this.totalPages
+    ) {
+      this.page++;
+      this.obterTopicos();
+    }
+  }
+
+  isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isVisible;
   }
 
 }
